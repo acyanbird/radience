0fef5e79ed49118dab958575dc539baa575d32ca
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2019-01-02 17:14:18 +0800

    almost finished pdf spliting
    produce pdf files for each individual question. batch process allowed

--- pypdf/split.py
+++ pypdf/split.py
@@ -7,19 +7,41 @@ from io import StringIO
 def find_question(location):
     obj = open(location, 'rb')
     reader = PyPDF4.PdfFileReader(obj)
+    index = 1
+    results = []
     for page in range(reader.numPages):
         page_obj = reader.getPage(page)
         pages_text = page_obj.extractText()
         lines = StringIO(pages_text).readlines()
         for line in range(len(lines)):
-            for m in re.finditer(r"", lines[line]):
-                return [page, line, m.group(0)]
+            for m in re.finditer(str(index)+"\t", lines[line]):
+                if m.start() == 0:
+                    results.append([page, index])
+                    index += 1
+    return results
 
 
 def creat_new_folder(new, inside):
     needed = inside + "\\" + new
     if not os.path.exists(needed):
         os.makedirs(needed)
+
+
+def cutting(paper):
+    starts = find_question(paper)
+    source = PyPDF4.PdfFileReader(paper)
+    output = []
+    for index in range(len(starts)-1):
+        tem = PyPDF4.PdfFileWriter()
+        for page in range(starts[index][0],starts[index+1][0]):
+            tem.addPage(source.getPage(page))
+        output.append(tem)
+    tem = PyPDF4.PdfFileWriter()
+    for page in range(starts[len(starts)][0], source.numPages):
+        tem.addPage(source.getPage(page))
+    output.append(tem)
+
+
 
 
 path = "cs"
